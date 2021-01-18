81599b9eaff3e65ee7f1ebba714f33d8b2e65067
Francesco <francesco.paolucci.93@gmail.com>
2021-01-18 15:19:00 +0100

    c4o:hasContent quote

--- public/kwic.js
+++ public/kwic.js
@@ -47,7 +47,7 @@ var kwic = new (function () {
 	const rdfaQuote = {
 		typeof: 'moro:Quotation',
 		property: 'dcterms:relation',
-		label: 'c4o:asContent'
+		label: 'c4o:hasContent'
 	}
 	
 	// generates the id for a mention
@@ -1250,12 +1250,15 @@ var kwic = new (function () {
 		}
 
 		// Reduce label.
-		let words = this.label.split(' ');
-		let reduce_words = words.slice(0,6);
+		if(this.label){
+			console.log(this.label)
+			let words = this.label.split(' ');
+			let reduce_words = words.slice(0,6);
 
-		if(words.length > 6) reduce_words.push('...');
-		
-		this.label = reduce_words.join(' ');
+			if(words.length > 6) reduce_words.push('...');
+			
+			this.label = reduce_words.join(' ');
+		}		
 		 
 		return this; 	
 	}
@@ -1455,7 +1458,7 @@ var kwic = new (function () {
 		this.citation = this.node.attributes.about.value.replace(uriRegExp,'');
 		this.quote_text = this.node.getElementsByClassName('quote-text')[0];
 
-		if (this.node.attributes.content) this.label = this.node.attributes.content // this is the value used for displaying the entity this mention belongs to
+		if (this.node.attributes.content) this.label = this.node.attributes.content.value // this is the value used for displaying the entity this mention belongs to
 		if (dataset.sort) this.sort = dataset.sort // this is the value used for sorting the entity this mention belongs to
 		if (this.node.attributes.resource) this.footnoteID = this.node.attributes.resource.value
 		if ($(this.footnoteID).length) this.footnoteText = $(this.footnoteID).text()		 
