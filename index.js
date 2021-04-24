3f41c910a0e2e5cfd98b809949c9c9cd287665d7
Francesco <francesco.paolucci.93@gmail.com>
2021-04-24 14:39:42 +0200

    /marcatura2

--- index.js
+++ index.js
@@ -29,27 +29,27 @@ app.set('views', './public')
 app.engine('html', require('ejs').renderFile);
 
 
-app.get('marcatura/index', verify, (req,res) => {
-    res.render('index.html')
+app.get('/marcatura/index', verify, (req,res) => {
+res.render('index.html')
 })
 
-app.get('marcatura/login', (req,res) =>{
+app.get('/marcatura/login', (req,res) =>{
     res.render("login.html");
 })
 
-app.get('marcatura/documentazione', (req,res) => {
+app.get('/marcatura/documentazione', (req,res) => {
     res.render("documentation.html")
 })
 
-app.get('marcatura/send_email', (req,res) =>{
+app.get('/marcatura/send_email', (req,res) =>{
     res.render("send_email.html");
 })
 
-app.get('marcatura/', verify , function( req, res ) {
+app.get('/marcatura/', verify , function( req, res ) {
     res.redirect('/index')
    });
 
-app.get('marcatura/read', function(req,res){
+app.get('/marcatura/read', function(req,res){
     let file = req.query.file;
 
     fs.readFile(`./doc/${file}`, (err, data) => {
